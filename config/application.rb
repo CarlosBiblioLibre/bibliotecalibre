25224030b847edbaa7ff3e65d73cb62ca442090c
Rodrigo <ro.gonzalez.riquelme@gmail.com>
2013-10-24 20:49:41 -0300

    app.rb

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module BiblioLibre
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    # config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = false
 
     config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
