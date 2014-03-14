20492a473840a88392b95eeb5684a064279afcb6
Carlos Mancilla <sebbasstian@gmail.com>
2014-03-14 16:16:39 -0300

    remove migration duplicated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140314175654) do
+ActiveRecord::Schema.define(:version => 20140314191620) do
 
   create_table "battles", :force => true do |t|
     t.string   "title"
