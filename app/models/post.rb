f61bfee72a9ef80d26006dbc45aca3202d2c80d2
Carlos Mancilla <sebbasstian@gmail.com>
2014-03-28 10:11:51 -0300

    organizo application css

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,6 +21,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :description, :title, :picture, :small_picture, :tag_list, :kind, :bajada, :autor
   acts_as_taggable
+  acts_as_votable
   has_many :hearts
   has_attached_file :picture, styles: {
     thumb: '100x100>',
