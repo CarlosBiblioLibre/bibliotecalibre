3c0328bb7ae426d526b5052fc7e1580b4a3d84d4
Rodrigo Andrés González Riquelme <ro.gonzalez.riquelme@gmail.com>
2019-01-18 00:31:16 -0300

    config.assets.compile = false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ BiblioLibre::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false # Disables security vulnerability
 
   # Generate digests for assets URLs
   config.assets.digest = true
